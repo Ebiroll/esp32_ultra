cef5f4bd4527dd157d6d248211c15118fac52810
Olof Astrand (on Laptop) <olof.astrand@gmail.com>
2016-12-18 21:25:42 +0100

    Added ultrasonic sensor for ESP32

---
+++ README.md
@@ -0,0 +1,55 @@
+#Ultrasonic 3.3V sensor with ESP32 
+
+I got an ultrasonic sensor that actually works with 3.3V VCC.
+Note that a normal HC-SR04 sensor will not work. It requires 5V Vcc.
+If you want to use it is vital to reduce the echo to 3.3V for input.
+
+This sensor, however actually works on only 3.3V
+
+https://www.aliexpress.com/item/HC-SR04P-Ultrasonic-Ranging-Module-Ranging-Sensor-Module-3-5-5V-Wide-Voltage-Performance-Is-Stronger/32711959780.html
+
+I hooked it up to my ESP32 thing.
+
+I use pin 4 as echo and 15 as trig.
+#define ECHO_PIN GPIO_NUM_4
+#define TRIG_PIN GPIO_NUM_15
+
+
+https://www.sparkfun.com/products/13907
+
+![connection](connect.png)
+
+
+When running in qemu, the following ports are accessed.
+
+
+    gpio_pad_select_gpio(ECHO_PIN);
+    gpio_pad_select_gpio(TRIG_PIN);
+
+
+io read 49048 
+io write 49048,2000 
+io read 4903c 
+io write 4903c,2000 
+
+
+
+---------------
+
+     gpio_set_level(TRIG_PIN, 1);
+     vTaskDelay(100 / portTICK_PERIOD_MS);
+     gpio_set_level(TRIG_PIN, 0);
+     uint32_t startTime=get_usec();
+
+
+io write 4903c,0 
+io write 44024,8000 
+io read 440c4 
+io write 440c4,0 
+io read 49048 
+io write 49048,200 
+io write 44028,10 
+io read 44098 
+io write 44098,0 
+
+
--- main/main.c
+++ main/main.c
@@ -5,15 +5,40 @@
 #include "esp_event_loop.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
+#include <sys/time.h>
+
+#define ECHO_PIN GPIO_NUM_4
+#define TRIG_PIN GPIO_NUM_15
+
 
 esp_err_t event_handler(void *ctx, system_event_t *event)
 {
     return ESP_OK;
 }
 
+// Similar to uint32_t system_get_time(void)
+uint32_t get_usec() {
+
+ struct timeval tv;
+
+ //              struct timeval {
+ //                time_t      tv_sec;     // seconds 
+ //                suseconds_t tv_usec;    // microseconds 
+ //              };
+
+ gettimeofday(&tv,NULL);
+ return (tv.tv_sec*1000000 + tv.tv_usec);
+
+
+  //uint64_t tmp=get_time_since_boot();
+  //uint32_t ret=(uint32_t)tmp;
+  //return ret;
+}
+
 void app_main(void)
 {
     nvs_flash_init();
+#if 0
     tcpip_adapter_init();
     ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
@@ -30,13 +55,51 @@ void app_main(void)
     ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
     ESP_ERROR_CHECK( esp_wifi_start() );
     ESP_ERROR_CHECK( esp_wifi_connect() );
+#endif
 
-    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
-    int level = 0;
+
+
+    gpio_pad_select_gpio(TRIG_PIN);
+    gpio_pad_select_gpio(ECHO_PIN);
+
+    gpio_set_direction(TRIG_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_direction(ECHO_PIN, GPIO_MODE_INPUT);
+
+
+    //int level = 0;
     while (true) {
-        gpio_set_level(GPIO_NUM_4, level);
-        level = !level;
-        vTaskDelay(300 / portTICK_PERIOD_MS);
+        gpio_set_level(TRIG_PIN, 1);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        gpio_set_level(TRIG_PIN, 0);
+        uint32_t startTime=get_usec();
+
+        while (gpio_get_level(ECHO_PIN)==0 && get_usec()-startTime < 500*1000)
+        {
+            // Wait until echo goes high
+        }
+
+        startTime=get_usec();
+
+        while (gpio_get_level(ECHO_PIN)==1 && get_usec()-startTime < 500*1000)
+        {
+            // Wait until echo goes low again
+        }
+
+        if (gpio_get_level(ECHO_PIN) == 0)
+        {
+            uint32_t diff = get_usec() - startTime; // Diff time in uSecs
+            // Distance is TimeEchoInSeconds * SpeeOfSound / 2
+            double distance = 340.29 * diff / (1000 * 1000 * 2); // Distance in meters
+            printf("Distance is %f cm\n", distance * 100);
+        }
+        else
+        {
+            // No value
+            printf("Did not receive a response!\n");
+        }
+        // Delay and re run.
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
+
 }
 
